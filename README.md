91579ed98b8bf29102c80289065880509bf810ac
Gutierrezfila <User@DESKTOP-D4JTUSA>
2024-07-24 10:07:14 -0600

    proyecto 1

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # progra-1-2024
 clases y ejemplos de programacion computacional -1 clase virtual
+//ejercicio de suma de dos numeros introducidos por el usuario
+            Console.Write("num 1:");
+            int num1 = int.Parse(Console.ReadLine()); //"5"-> 5
+
+            Console.Write("num 2:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int repuesta = num1 + num2;
+            Console.WriteLine("la suma de {0}+{1}={2}"), num1, num2, respuesta
+
+            //pausa
+            console.ReadLine();
---
+++ mi primer proyecto/mi primer proyecto/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Activities;
+using System.Activities.Statements;
+using System.Linq;
+
+namespace mi_primer_proyecto
+    {  internal class program {
+        static void Main(string[] args)
+        {
+            //prioridad de los operadores aritmeticos
+            //ejercicio obtener el promedio de una serie de numeros
+            int[] serie = new int[] { 5, 4, 6, 8, 9 };
+            int suma = 0;
+            foreach (int num in serie) {
+                suma += num;
+
+            }
+            double prom = suma + suma / serie.Length;
+            Console.WriteLine("la suma es : {0}, el promedio {1}", suma, prom);
+            // pausa
+            Console.ReadLine();
+            
+
+            
+
+            
+        }
+    }
+}
